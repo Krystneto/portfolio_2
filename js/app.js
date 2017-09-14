c6524b2ccfc8c4f6f84311ba3d03edf6ec944724
Kristopher Parrillo <kkparrillo@gmail.com>
2017-09-13 17:33:09 -0700

    adds color

--- js/app.js
+++ js/app.js
@@ -1,9 +1,20 @@
 const aboutUs = document.body.querySelector('.about_us');
+const aboutMeContainer = document.body.querySelector('.about-me-container--circle');
+const btnTop = document.body.querySelector('.btn-top')
 
-const handleClick = (event) => aboutUs.classList.toggle('complement-color');
-    
-const aboutMeImage = document.body.querySelector('.about-me-container--circle');
-    
-const hello = aboutMeImage.addEventListener('click', handleClick);
+const handleClick = () => {
+    aboutUs.classList.toggle('complement-color');
+    aboutMeContainer.classList.toggle('rollOut');
+    setTimeout(() => aboutMeContainer.classList.remove('rollIn', 'rollOut'), 1000)
+}
 
+const handleImageFadeIn = () => 
+    setTimeout(() => {
+        aboutMeContainer.classList.toggle('animated')
+        aboutMeContainer.classList.toggle('rollIn')
+    }
+    ,250)
 
+aboutMeContainer.addEventListener('click', handleClick);
+
+btnTop.addEventListener('click', handleImageFadeIn);
